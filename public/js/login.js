9cb75ada649321e80b523ffada8320a0ee6a3e61
Sree Nithya Reddy Peesu <22cs01030@iitbbs.ac.in>
2025-07-19 01:11:22 +0530

    implemented signup button

--- public/js/login.js
+++ public/js/login.js
@@ -21,6 +21,30 @@ export const login = async(email,password)=>{
     }
 };
 
+export const signup = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
+      }
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Account created successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1000);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
+
 export const logout = async() =>{
   try{
     const res = await axios({
