ef279b2de8a9da21e226fbc2c174793a59494dea
Sree Nithya Reddy Peesu <22cs01030@iitbbs.ac.in>
2025-07-19 00:05:13 +0530

    corrected error in stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,7 +53,7 @@ const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const userdoc = await User.findOne({email: session.customer_email});
     const user = userdoc._id;
-    const price = session.line_items[0].price_data.unit_amount/100;
+    const price = session.amount_total/100;
     await Booking.create({tour,user,price});
 };
 
